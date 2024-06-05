a643d196ee1573186d1505564521703e21506ccb
Prince Vasoya <princevasoya@Princes-MacBook-Pro.local>
2024-06-05 14:03:47 +0530

    v1

--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -1,7 +1,88 @@
 
 
-import  { useState } from "react";
-// import { Link } from "react-router-dom";
+// import  { useState } from "react";
+// // import { Link } from "react-router-dom";
+// import { ICharacter } from "../store";
+// import IconPlanet from "./IconPlanet";
+// import { HTMLAttributes } from "react";
+// import FavoriteIcon from "./FavoriteIcon";
+
+// interface CharacterCardElement extends HTMLAttributes<HTMLDivElement> {
+//   character: ICharacter;
+// }
+
+// export default function CharacterCard(props: CharacterCardElement) {
+//   const [imageLoaded, setImageLoaded] = useState(false);
+
+//   const handleImageLoad = () => {
+//     setImageLoaded(true);
+//   };
+
+//   const handleClick = () => {
+//     // Add a delay before routing to the characters page
+//     setTimeout(() => {
+//       window.location.href = `/character/${props.character.id}#body`;
+//     }, 300); // Adjust the delay time as needed (500 milliseconds in this example)
+//   };
+
+//   return (
+//     <div className={`card relative h-fit ${props.className}`}>
+//       <article className="flex flex-col gap-2 card-content">
+//         {/* image */}
+//         <div className="flex rounded-md justify-center items-center h-36 overflow-hidden">
+//           <img
+//             className={`w-full h-full object-contain ${!imageLoaded ? "lazyload" : ""}`}
+//             loading="lazy"
+//             alt={props.character.name}
+//             src={props.character.image}
+//             onLoad={handleImageLoad}
+//           />
+//           {!imageLoaded && (
+//             <div className="w-full h-full flex justify-center items-center bg-gray-200">
+//               Loading...
+//             </div>
+//           )}
+//         </div>
+
+//         {/* header */}
+//         <header className="flex gap-2 justify-between items-center">
+//           <h4 className="font-bold text-md whitespace-nowrap text-ellipsis overflow-hidden">
+//             {props.character.name}
+//           </h4>
+//           <FavoriteIcon character={props.character} />
+//         </header>
+
+//         {/* info */}
+//         <article>
+//           <p className="w-full text-ellipsis overflow-hidden whitespace-nowrap">
+//             <i className="bi-person mr-2"></i>
+//             {props.character.gender}
+//           </p>
+//           <p>
+//             <i className="bi-activity mr-2"></i>
+//             {props.character.status}
+//           </p>
+//           <p className="w-full text-ellipsis overflow-hidden whitespace-nowrap">
+//             <i className="bi-person mr-2"></i>
+//             {props.character.species}
+//           </p>
+//           <p className="w-full text-ellipsis overflow-hidden whitespace-nowrap">
+//             <IconPlanet className="w-4 inline h-fit mr-2" />
+//             {props.character.origin.name}
+//           </p>
+//         </article>
+
+//         <button
+//           onClick={handleClick}
+//           className="btn btn-white w-full block mt-2 text-center"
+//         >
+//           View details
+//         </button>
+//       </article>
+//     </div>
+//   );
+// }
+import { Link } from "react-router-dom";
 import { ICharacter } from "../store";
 import IconPlanet from "./IconPlanet";
 import { HTMLAttributes } from "react";
@@ -12,49 +93,35 @@ interface CharacterCardElement extends HTMLAttributes<HTMLDivElement> {
 }
 
 export default function CharacterCard(props: CharacterCardElement) {
-  const [imageLoaded, setImageLoaded] = useState(false);
-
-  const handleImageLoad = () => {
-    setImageLoaded(true);
-  };
-
-  const handleClick = () => {
-    // Add a delay before routing to the characters page
-    setTimeout(() => {
-      window.location.href = `/character/${props.character.id}`;
-    }, 300); // Adjust the delay time as needed (500 milliseconds in this example)
-  };
-
   return (
     <div className={`card relative h-fit ${props.className}`}>
       <article className="flex flex-col gap-2 card-content">
         {/* image */}
         <div className="flex rounded-md justify-center items-center h-36 overflow-hidden">
           <img
-            className={`w-full h-full object-contain ${!imageLoaded ? "lazyload" : ""}`}
             loading="lazy"
             alt={props.character.name}
             src={props.character.image}
-            onLoad={handleImageLoad}
           />
-          {!imageLoaded && (
-            <div className="w-full h-full flex justify-center items-center bg-gray-200">
-              Loading...
-            </div>
-          )}
         </div>
 
         {/* header */}
-        <header className="flex gap-2 justify-between items-center">
+        {/* <Link
+          to={`/character/${props.character.id}`}
+          className="btn btn-white w-full block mt-2 text-center"
+        > */}
+          <header className="flex gap-2 justify-between items-center">
           <h4 className="font-bold text-md whitespace-nowrap text-ellipsis overflow-hidden">
             {props.character.name}
           </h4>
           <FavoriteIcon character={props.character} />
         </header>
 
+        {/* </Link> */}
+
         {/* info */}
         <article>
-          <p className="w-full text-ellipsis overflow-hidden whitespace-nowrap">
+        <p className="w-full text-ellipsis overflow-hidden whitespace-nowrap">
             <i className="bi-person mr-2"></i>
             {props.character.gender}
           </p>
@@ -72,12 +139,12 @@ export default function CharacterCard(props: CharacterCardElement) {
           </p>
         </article>
 
-        <button
-          onClick={handleClick}
+        <Link
+          to={`/character/${props.character.id}`}
           className="btn btn-white w-full block mt-2 text-center"
         >
           View details
-        </button>
+        </Link>
       </article>
     </div>
   );
