8dbac028c238878f53d1b39e312d98b6e3670da6
Prince Vasoya <princevasoya@Princes-MacBook-Pro.local>
2024-06-05 14:24:21 +0530

    v1

--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -1,88 +1,5 @@
-
-
-// import  { useState } from "react";
-// // import { Link } from "react-router-dom";
-// import { ICharacter } from "../store";
-// import IconPlanet from "./IconPlanet";
-// import { HTMLAttributes } from "react";
-// import FavoriteIcon from "./FavoriteIcon";
-
-// interface CharacterCardElement extends HTMLAttributes<HTMLDivElement> {
-//   character: ICharacter;
-// }
-
-// export default function CharacterCard(props: CharacterCardElement) {
-//   const [imageLoaded, setImageLoaded] = useState(false);
-
-//   const handleImageLoad = () => {
-//     setImageLoaded(true);
-//   };
-
-//   const handleClick = () => {
-//     // Add a delay before routing to the characters page
-//     setTimeout(() => {
-//       window.location.href = `/character/${props.character.id}#body`;
-//     }, 300); // Adjust the delay time as needed (500 milliseconds in this example)
-//   };
-
-//   return (
-//     <div className={`card relative h-fit ${props.className}`}>
-//       <article className="flex flex-col gap-2 card-content">
-//         {/* image */}
-//         <div className="flex rounded-md justify-center items-center h-36 overflow-hidden">
-//           <img
-//             className={`w-full h-full object-contain ${!imageLoaded ? "lazyload" : ""}`}
-//             loading="lazy"
-//             alt={props.character.name}
-//             src={props.character.image}
-//             onLoad={handleImageLoad}
-//           />
-//           {!imageLoaded && (
-//             <div className="w-full h-full flex justify-center items-center bg-gray-200">
-//               Loading...
-//             </div>
-//           )}
-//         </div>
-
-//         {/* header */}
-//         <header className="flex gap-2 justify-between items-center">
-//           <h4 className="font-bold text-md whitespace-nowrap text-ellipsis overflow-hidden">
-//             {props.character.name}
-//           </h4>
-//           <FavoriteIcon character={props.character} />
-//         </header>
-
-//         {/* info */}
-//         <article>
-//           <p className="w-full text-ellipsis overflow-hidden whitespace-nowrap">
-//             <i className="bi-person mr-2"></i>
-//             {props.character.gender}
-//           </p>
-//           <p>
-//             <i className="bi-activity mr-2"></i>
-//             {props.character.status}
-//           </p>
-//           <p className="w-full text-ellipsis overflow-hidden whitespace-nowrap">
-//             <i className="bi-person mr-2"></i>
-//             {props.character.species}
-//           </p>
-//           <p className="w-full text-ellipsis overflow-hidden whitespace-nowrap">
-//             <IconPlanet className="w-4 inline h-fit mr-2" />
-//             {props.character.origin.name}
-//           </p>
-//         </article>
-
-//         <button
-//           onClick={handleClick}
-//           className="btn btn-white w-full block mt-2 text-center"
-//         >
-//           View details
-//         </button>
-//       </article>
-//     </div>
-//   );
-// }
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { ICharacter } from "../store";
 import IconPlanet from "./IconPlanet";
 import { HTMLAttributes } from "react";
@@ -93,6 +10,19 @@ interface CharacterCardElement extends HTMLAttributes<HTMLDivElement> {
 }
 
 export default function CharacterCard(props: CharacterCardElement) {
+  const [isNavigating, setIsNavigating] = useState(false);
+  const navigate = useNavigate();
+
+  const handleLinkClick = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+    event.preventDefault();
+    if (!isNavigating) {
+      setIsNavigating(true);
+      setTimeout(() => {
+        navigate(`/character/${props.character.id}`);
+      }, 300); // 300 milliseconds delay
+    }
+  };
+
   return (
     <div className={`card relative h-fit ${props.className}`}>
       <article className="flex flex-col gap-2 card-content">
@@ -102,31 +32,26 @@ export default function CharacterCard(props: CharacterCardElement) {
             loading="lazy"
             alt={props.character.name}
             src={props.character.image}
+            className="w-full h-full object-cover"
           />
         </div>
 
         {/* header */}
-        {/* <Link
-          to={`/character/${props.character.id}`}
-          className="btn btn-white w-full block mt-2 text-center"
-        > */}
-          <header className="flex gap-2 justify-between items-center">
+        <header className="flex gap-2 justify-between items-center">
           <h4 className="font-bold text-md whitespace-nowrap text-ellipsis overflow-hidden">
             {props.character.name}
           </h4>
           <FavoriteIcon character={props.character} />
         </header>
 
-    
-
         {/* info */}
         <article>
-        <p className="w-full text-ellipsis overflow-hidden whitespace-nowrap">
+          <p className="w-full text-ellipsis overflow-hidden whitespace-nowrap">
             <i className="bi-person mr-2"></i>
             {props.character.gender}
           </p>
           <p>
-            <i className="bi-activity mr-2 "></i>
+            <i className="bi-activity mr-2"></i>
             {props.character.status}
           </p>
           <p className="w-full text-ellipsis overflow-hidden whitespace-nowrap">
@@ -141,6 +66,7 @@ export default function CharacterCard(props: CharacterCardElement) {
 
         <Link
           to={`/character/${props.character.id}`}
+          onClick={handleLinkClick}
           className="btn btn-white w-full block mt-2 text-center"
         >
           View details
@@ -149,3 +75,71 @@ export default function CharacterCard(props: CharacterCardElement) {
     </div>
   );
 }
+
+// import { Link } from "react-router-dom";
+// import { ICharacter } from "../store";
+// import IconPlanet from "./IconPlanet";
+// import { HTMLAttributes } from "react";
+// import FavoriteIcon from "./FavoriteIcon";
+
+// interface CharacterCardElement extends HTMLAttributes<HTMLDivElement> {
+//   character: ICharacter;
+// }
+
+// export default function CharacterCard(props: CharacterCardElement) {
+//   return (
+//     <div className={`card relative h-fit ${props.className}`}>
+//       <article className="flex flex-col gap-2 card-content">
+//         {/* image */}
+//         <div className="flex rounded-md justify-center items-center h-36 overflow-hidden">
+//           <img
+//             loading="lazy"
+//             alt={props.character.name}
+//             src={props.character.image}
+//           />
+//         </div>
+
+//         {/* header */}
+//         {/* <Link
+//           to={`/character/${props.character.id}`}
+//           className="btn btn-white w-full block mt-2 text-center"
+//         > */}
+//           <header className="flex gap-2 justify-between items-center">
+//           <h4 className="font-bold text-md whitespace-nowrap text-ellipsis overflow-hidden">
+//             {props.character.name}
+//           </h4>
+//           <FavoriteIcon character={props.character} />
+//         </header>
+
+    
+
+//         {/* info */}
+//         <article>
+//         <p className="w-full text-ellipsis overflow-hidden whitespace-nowrap">
+//             <i className="bi-person mr-2"></i>
+//             {props.character.gender}
+//           </p>
+//           <p>
+//             <i className="bi-activity mr-2 "></i>
+//             {props.character.status}
+//           </p>
+//           <p className="w-full text-ellipsis overflow-hidden whitespace-nowrap">
+//             <i className="bi-person mr-2"></i>
+//             {props.character.species}
+//           </p>
+//           <p className="w-full text-ellipsis overflow-hidden whitespace-nowrap">
+//             <IconPlanet className="w-4 inline h-fit mr-2" />
+//             {props.character.origin.name}
+//           </p>
+//         </article>
+
+//         <Link
+//           to={`/character/${props.character.id}`}
+//           className="btn btn-white w-full block mt-2 text-center"
+//         >
+//           View details
+//         </Link>
+//       </article>
+//     </div>
+//   );
+// }
